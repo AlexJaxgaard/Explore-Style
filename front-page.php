72405b81dd70b8df84262f42a4cf71c87df7e582
Alexander Jaxgård <snalexdci@hotmail.com>
2021-09-08 14:06:19 +0200

    Sätter text på startsidan

--- front-page.php
+++ front-page.php
@@ -1,6 +1,16 @@
 <?php get_header(); ?>
 
 
-
+<section class="hero">
+        <a href="#" class="hero__wrapper">
+            <div class="wrapper">
+                <div class="wrapper__heading">
+                    <span>THE NEW COLLECTION</span>
+                    <h1>Elegant & Luxury</h1>
+                    <div class="button button-white">BROWSE COLLECTION</div>
+                </div>
+            </div>
+        </a>
+    </section>
 
 <?php get_footer(); ?>
