17b31060ebe48d9f597e88fa0251ef1369d7ec3e
Alexander Jaxgård <snalexdci@hotmail.com>
2021-09-10 12:23:28 +0200

    Update front-page.php
    Lägger till länk till /shop när användaren trycker på bild/text
    
    Lägger till nya element under wrappern.

--- front-page.php
+++ front-page.php
@@ -2,7 +2,7 @@
 
 
 <section class="hero">
-        <a href="#" class="hero__wrapper">
+        <a href="/shop" class="hero__wrapper">
             <div class="wrapper">
                 <div class="wrapper__heading">
                     <span>THE NEW COLLECTION</span>
@@ -12,5 +12,36 @@
             </div>
         </a>
     </section>
+
+    <section class="standard-promo">
+
+<a href="/promo" class="promo-card promo-card--gray promo-card--women">
+    <h2 class="promo-card__heading">Shop Women</h2>
+    <p class="promo-card__body">Women's new arrivals. It's time to explore your options.</p>
+</a>
+
+<a href="#" class="promo-card promo-card--gray promo-card--men">
+    <h2 class="promo-card__heading">Shop Men</h2>
+    <p class="promo-card__body">Mens's new arrivals. It's time to explore your options.</p>
+</a>
+
+</section>
+
+
+<section class="shop-section">
+<h2 class="shop-section__heading">Women's Best Sellers</h2>
+<p class="shop-section__body">Our women's bestsellers. They are smooth and soft<br/> with adjustable elastic loop.</p>
+<div class="items">
+    Items
+</div>
+</section>
+
+<section class="shop-section">
+<h2 class="shop-section__heading">New Arrivals</h2>
+<p class="shop-section__body">New arrivals. Updated every<br/> day. It's time to explore.</p>
+<div class="items">
+    Items
+</div>
+</section>
 
 <?php get_footer(); ?>
