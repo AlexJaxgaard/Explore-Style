8079ff3f4f3d02cbe01c0c0fecce918bdc8dd31c
Alexander Jaxgård <snalexdci@hotmail.com>
2021-09-28 13:30:29 +0200

    Update 404.php
    Skapade en 404-sida som visas när en sida som inte existerar försöker kommas åt. Använder header och footer från header.php respektive footer.php och skapar en ruta för bilden.

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<main>
+
+
+
+    <div class="page-hero" style="background-image:url('https://wallpapercrafter.com/dwn.php?f=6583-man-city-loneliness-fog-street-4k.jpg')">
+        <h1 class="page-hero__heading">404 - Error<?php the_title(); ?></h1>
+        
+    </div>
+    <h3 style="text-align:center;">Looks like this page doesn't exist. Sorry about that.</h3>
+    <?php the_content(); ?>    
+
+</main>
+
+<?php get_footer(); ?>
