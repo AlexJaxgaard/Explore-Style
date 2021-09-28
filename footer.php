bbed139399d4c9943917f8e8283d1d63d86d2958
Alexander Jaxgård <snalexdci@hotmail.com>
2021-09-28 13:49:42 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 
 <?php 
 wp_nav_menu(array(
-'menu' => '20', 
+'menu' => '23', //Change this to the correct menu no.
 'menu_class' => 'footer-container__body'
 
 
@@ -18,7 +18,7 @@ wp_nav_menu(array(
                 <h3 class="footer-container__heading">Useful Info <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg></h3>
                 <?php 
 wp_nav_menu(array(
-'menu' => '24', 
+'menu' => '24', //Change this to the correct menu no.
 'menu_class' => 'footer-container__body'
 
 
@@ -29,7 +29,7 @@ wp_nav_menu(array(
                 <h3 class="footer-container__heading">Company <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg></h3>
                 <?php 
 wp_nav_menu(array(
-'menu' => '25', 
+'menu' => '25', //Change this to the correct menu no.
 'menu_class' => 'footer-container__body'
 
 
