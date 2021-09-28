fa07596d892a246bb837dbf27a6a1cad3b5faa20
Alexander Jaxgård <snalexdci@hotmail.com>
2021-09-28 12:44:23 +0200

    Update front-page.php
    Bytte ut text, tog bort vissa element då jag inte anser att de passar in på sidan

--- front-page.php
+++ front-page.php
@@ -2,35 +2,23 @@
 
 
 <section class="hero">
-        <a href="/shop" class="hero__wrapper">
+        <a href="/product-category/casual/" class="hero__wrapper">
             <div class="wrapper">
                 <div class="wrapper__heading">
                     <span>THE NEW COLLECTION</span>
-                    <h1>Elegant & Luxury</h1>
-                    <div class="button button-white">BROWSE COLLECTION</div>
+                    <h1>Classy Casual</h1>
+                    <div class="button button-white">EXPLORE NOW</div>
                 </div>
             </div>
         </a>
     </section>
 
-    <section class="standard-promo">
 
-<a href="/promo" class="promo-card promo-card--gray promo-card--women">
-    <h2 class="promo-card__heading">Shop Women</h2>
-    <p class="promo-card__body">Women's new arrivals. It's time to explore your options.</p>
-</a>
-
-<a href="#" class="promo-card promo-card--gray promo-card--men">
-    <h2 class="promo-card__heading">Shop Men</h2>
-    <p class="promo-card__body">Mens's new arrivals. It's time to explore your options.</p>
-</a>
-
-</section>
 
 
 <section class="shop-section">
 <h2 class="shop-section__heading">Best Sellers</h2>
-<p class="shop-section__body">Our women's bestsellers. They are smooth and soft<br/> with adjustable elastic loop.</p>
+<p class="shop-section__body">Explore the hottest trends. Always up to date<br/> and high quality.</p>
 <div class="items">
     <?php echo do_shortcode(' [best_selling_products] ') ?>
 </div>
