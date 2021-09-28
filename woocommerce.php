7dd66db1ba91e654e032f0911bb4deeddd43c4f3
Alexander Jaxgård <snalexdci@hotmail.com>
2021-09-28 13:30:56 +0200

    Update woocommerce.php
    Lägger in en bild på toppen av sidan så att den följer temat på resten av undersidorna.

--- woocommerce.php
+++ woocommerce.php
@@ -1,6 +1,15 @@
 <?php get_header(); ?>
 
 <main>
+<div class="page-hero" style="background-image:url('https://wallpaper.dog/download-wallpaper/10907455_clothing-store-wallpapers'); ?>')">
+         <h1 class="page-hero__heading">Shop</h1> 
+        
+    </div>
+    
+ 
+
+
+
 <?php woocommerce_content(); ?>
 
 </main>
