a07890291437f3db883c8076332f95063a011b54
Alexander Jaxgård <snalexdci@hotmail.com>
2022-01-19 10:25:03 +0100

    Update woocommerce.php

--- woocommerce.php
+++ woocommerce.php
@@ -11,8 +11,8 @@
 <?php get_header(); ?>
 
 <main>
-<div class="page-hero" style="background-image:url('https://wallpaper.dog/download-wallpaper/10907455_clothing-store-wallpapers'); ?>')">
-         <h1 class="page-hero__heading">Shop</h1> 
+<div class="page-hero" style="background-image:url('/wp-content/themes/Explore-Style/images/cart.jpg'); ?>')">
+         <h1 class="page-hero__heading_shop">Shop</h1> 
         
     </div>
     
